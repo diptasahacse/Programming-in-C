ad1f96f95e4c9119da8f0633fff837579209bd7a
Dipta Saha <32943396+diptasahacse@users.noreply.github.com>
2019-03-01 22:18:28 +0530

    read the file using getc()

---
+++ readfile.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+  FILE *file1;
+
+    file1 = fopen("text.txt","r");
+    char ch;
+
+
+
+
+    if(file1 == NULL)
+    {
+        printf("File Dosenot exist");
+
+    }
+    else
+    {
+        printf("File Created\n");
+
+        while(!feof(file1))
+        {
+            ch = getc(file1);
+            printf("%c",ch);
+
+        }
+
+
+        printf("\nFile Read Successfully");
+        fclose(file1);
+
+
+    }
+
+
+
+    getch();
+}
