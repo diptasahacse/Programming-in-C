17c25af79dd9d78c252783ec9efcbddc14b704d0
Dipta Saha <32943396+diptasahacse@users.noreply.github.com>
2019-03-01 21:52:32 +0530

    writing to a file using puts() putstring

--- writingtoafile.c
+++ writingtoafile.c
@@ -3,8 +3,8 @@ int main()
 {
   FILE *file1;
 
-    file1 = fopen("text.txt","a"); //when i using a mode..if any file was not cteated,then new file will be create..if any file was already cteated then it wile be write from end line in the file
-    char c[20] = " Keya Saha";
+    file1 = fopen("text.txt","w"); //when i using a mode..if any file was not cteated,then new file will be create..if any file was already cteated then it wile be write from end line in the file
+    char c[20];
 
     int length = strlen(c);
 
@@ -16,13 +16,11 @@ int main()
     else
     {
         printf("File Created\n");
-
-        for(int i = 0;i < length;i++)
-        {
-            fputc(c[i],file1);
+        printf("Enter Your Name =");
+        gets(c);    //get data from user
 
 
-        }
+        fputs(c,file1); //put string
         printf("File Write Created");
         fclose(file1);
 
@@ -33,3 +31,4 @@ int main()
 
     getch();
 }
+
