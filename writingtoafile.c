045d9b8c352f999d3a2779c91ce52e86e77ad8e2
Dipta Saha <32943396+diptasahacse@users.noreply.github.com>
2019-03-01 21:41:45 +0530

    file put char fputc(); a mode

--- writingtoafile.c
+++ writingtoafile.c
@@ -3,8 +3,8 @@ int main()
 {
   FILE *file1;
 
-    file1 = fopen("text.txt","w"); //when i using w mode..if any file was not cteated,then new file will be create..if any file was already cteated then it wile be overrite what ever before written;
-    char c[20] = "Dipta Saha";
+    file1 = fopen("text.txt","a"); //when i using a mode..if any file was not cteated,then new file will be create..if any file was already cteated then it wile be write from end line in the file
+    char c[20] = " Keya Saha";
 
     int length = strlen(c);
 
